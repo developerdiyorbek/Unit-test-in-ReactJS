46840e727564481134a0fcfc635f2ac7a01ab597
developerdiyorbek <developersulaymonov1907@gmail.com>
2024-08-12 21:32:01 +0500

    install react

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+// Testing types : unit/functional, integration, e2e(end-two-end), visual
+// Testing libraries : mocha, jasmine, jest, vitest, node, bun
+
+function App() {
+  return <div>App</div>;
+}
+
+export default App;
